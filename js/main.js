82ad91537618862168857a534e6db817717d133d
Alekx2211 <42638496+Alekx2211@users.noreply.github.com>
2022-05-18 17:35:49 +0300

    12

--- js/main.js
+++ js/main.js
@@ -8,3 +8,13 @@ var swiper = new Swiper(".worst", {
         prevEl: ".prev1",
     },
 });
+var swiper = new Swiper(".board", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    loop: true,
+
+    navigation: {
+        nextEl: ".next1",
+        prevEl: ".prev1",
+    },
+});
